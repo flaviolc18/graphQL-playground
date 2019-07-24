99219a2cc698d41472e7cfdbc729751fc2645037
= <flaviolc18@gmail.com>
2019-07-24 19:13:06 -0300

    feat: autentication with jwt

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,7 +2,10 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { withRouter } from "react-router";
 
-function Header() {
+const { AUTH_TOKEN } = require("../constants");
+
+function Header({ history }) {
+  const authToken = localStorage.getItem(AUTH_TOKEN);
   return (
     <div className="flex pa1 justify-between nowrap orange">
       <div className="flex flex-fixed black">
@@ -10,10 +13,31 @@ function Header() {
         <Link to="/" className="ml1 no-underline black">
           new
         </Link>
-        <div className="ml1">|</div>
-        <Link to="/create" className="ml1 no-underline black">
-          submit
-        </Link>
+        {authToken && (
+          <div className="flex">
+            <div className="ml1">|</div>
+            <Link to="/create" className="ml1 no-underline black">
+              submit
+            </Link>
+          </div>
+        )}
+      </div>
+      <div className="flex flex-fixed">
+        {authToken ? (
+          <div
+            className="ml1 pointer black"
+            onClick={() => {
+              localStorage.removeItem(AUTH_TOKEN);
+              history.push(`/`);
+            }}
+          >
+            logout
+          </div>
+        ) : (
+          <Link to="/login" className="ml1 no-underline black">
+            login
+          </Link>
+        )}
       </div>
     </div>
   );
