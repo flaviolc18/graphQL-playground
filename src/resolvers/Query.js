2a7546119518bdac795141cc93440a9f4933d19c
= <flaviolc18@gmail.com>
2019-07-24 15:27:02 -0300

    feat: pagination with skip and limit

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,3 +1,9 @@
-exports.feed = (parent, args, context, info) => {
-  return context.prisma.links();
+exports.feed = (parent, { filter, skip, first }, context, info) => {
+  const where = filter
+    ? {
+        OR: [{ description_contains: filter }, { url_contains: filter }]
+      }
+    : {};
+
+  return context.prisma.links({ where, skip, first });
 };
